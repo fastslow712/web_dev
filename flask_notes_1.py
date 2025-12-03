2237d766bc08e273ee8a2269085f425b8f9c4be8
fastslow712 <arthur.heaps@ucas-edu.net>
2025-12-03 14:41:50 -0700

    made notes

--- flask_notes_1.py
+++ flask_notes_1.py
@@ -1,28 +1,17 @@
 #flask notes: tutoreal
-#What does Flask do?
-#alows mutipule page websites
-
-#What are the steps to setting up a Flask project?
-#
-
-#How can you reference subpages on your Flask project? (Meaning the difference between the home page and a personal profile)
-#/name of page
-
-#What are templates?
-#alows you to make a website with mutipul html tags
-from flask import Flask, render_template, rwquest, rederect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    if request,method == "POST":
+    if request.method == "POST":
         return redirect(url_for("user", name=request.form["name"]))
     return render_template("index.html")
 
 @app.route("/contact")
 def contact():
-    return "<p>Don't contact me. I don't want to talk to you</p>"
+    return render_template("contact.html")
 
 @app.route("/<name>")
 def user(name):
